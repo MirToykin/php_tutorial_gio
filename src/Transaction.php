03f674966acccf3ef02b2b8af4a7f5f9f4cb79f8
Miroslav <mt@truefuture.io>
2022-08-01 10:36:23 +0300

    refactor

--- src/Transaction.php
+++ src/Transaction.php
@@ -4,13 +4,11 @@ declare(strict_types=1);
 
 class Transaction
 {
-    private float $amount;
-    private string $description;
-
-    public function __construct(float $amount, string $description)
+    public function __construct(
+        private float $amount,
+        private string $description
+    )
     {
-        $this->amount = $amount;
-        $this->description = $description;
     }
 
     public function addTax(float $rate): self
